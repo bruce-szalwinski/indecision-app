447a38436fd9e2df717da0935edaa82716538cb5
Bruce Szalwinski <szalwinski@csdisco.com>
2020-07-26 12:47:50 -0500

    remove unneeded

--- src/app.js
+++ src/app.js
@@ -92,17 +92,6 @@ class IndecisionApp extends React.Component {
     }
 }
 
-// class Header extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.props.title}</h1>
-//                 <h2>{this.props.subtitle}</h2>
-//             </div>
-//         )
-//     }
-// }
-
 const Header = (props) => {
     return (
         <div>
@@ -115,21 +104,6 @@ const Header = (props) => {
  Header.defaultProps = {
      title: "Indecision App"
  }
-
-// class Action extends React.Component {
-
-//     render() {
-//         return (
-//             <div>
-//                 <button
-//                     onClick={this.props.handlePickOption}
-//                     disabled={(!this.props.hasOptions)}
-//                 >What should i do?
-//                 </button>
-//             </div>
-//         )
-//     }
-// }
 
 const Action = (props) => {
     return (
