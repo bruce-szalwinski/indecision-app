9ef6c85c8166064b8ef3ae2c0c57ed927600a7bc
Bruce Szalwinski <szalwinski@csdisco.com>
2020-07-26 15:23:59 -0500

    webpack invokes babel with presets to convert es6

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
-import subtract, {square, add, isAdult, canDrink} from "./utils.js"
+import subtract, {square, add} from "./utils.js"
 import isSenior, {canDrink, isAdult} from "./person.js"
+import validator from 'validator'
+import React from 'react'
+import ReactDom from 'react-dom'
+
 
 console.log("app.js is running!!!!!")
-console.log(square(4))
-console.log(add(3,4))
-console.log(subtract(4,0))
-console.log(canDrink(4))
-console.log(isAdult(40))
-console.log(isSenior(100))
+console.log('square: ' + square(4))
+console.log('add: ' + add(3,4))
+console.log('subtract: ' + subtract(4,0))
+console.log('canDrink: ' + canDrink(4))
+console.log('isAdult: ' + isAdult(40))
+console.log('isSenior' + isSenior(100))
+console.log('isEmail: ' + validator.isEmail('foo@foo.com'))
+
+const template = <p>testing</p>
+
+ReactDom.render(template, document.getElementById('app'))
